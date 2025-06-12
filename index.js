af7d37a1442032b020ce27697eae44640ccc162a
Caleb Shim <calebshim248@gmail.com>
2025-06-12 15:45:44 -0400

    Add problem sending logic

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
 const User = require('./models/user');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const server = createServer(app);
@@ -15,6 +16,7 @@ const io = new Server(server);
 const userOfSocketId = new Map();
 const socketIdOfUser = new Map();
 const socketOfUser = new Map();
+const roomOfSocket = new Map();
 
 
 // users waiting for a match
@@ -22,6 +24,9 @@ const usersWaiting = [];
 
 // matches each user in a match to opponent
 const usersInMatch = new Map();
+
+// matches each room to its array of problems
+const problemDataOfRoom = new Map();
 
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
@@ -45,15 +50,15 @@ mongoose.connect('mongodb://localhost:27017/mathracer')
     });
 
 app.get('/', (req, res) => {
-    res.render('temp');
-    // const { login, loginFailed, rememberMe, pwNoMatch, usernameTaken } = req.query;
-    // res.render('login', {
-    //     login: (login != "false"),
-    //     loginFailed: (loginFailed == "true"),
-    //     pwNoMatch: (pwNoMatch == "true"),
-    //     rememberMe: (rememberMe == "true"),
-    //     usernameTaken: (usernameTaken == "true")
-    // });
+    // res.render('temp');
+    const { login, loginFailed, rememberMe, pwNoMatch, usernameTaken } = req.query;
+    res.render('login', {
+        login: (login != "false"),
+        loginFailed: (loginFailed == "true"),
+        pwNoMatch: (pwNoMatch == "true"),
+        rememberMe: (rememberMe == "true"),
+        usernameTaken: (usernameTaken == "true")
+    });
 });
 
 io.engine.use(sessionMiddleware);
@@ -64,10 +69,54 @@ function removeFromArr(arr, value) {
     arr.splice(idx, 1);
 }
 
+class Problem {
+    constructor(statement, answer) {
+        this.statement = statement;
+        this.answer = answer;
+        this.id = uuidv4();
+    }
+}
+class ProblemData {
+    constructor(problem) {
+        this.problem = problem;
+        this.solvedBy = 0;
+    }
+}
+
+function createProblem() {
+    const rand = Math.floor(Math.random() * 4);
+    if (rand == 0) {
+        // addition
+        const a = Math.floor(Math.random() * 1000 - 500);
+        const b = Math.floor(Math.random() * 1000 - 500);
+        return new Problem(`${a} + ${b}`, `${a + b}`);
+    } else if (rand == 1) {
+        // subtraction
+        const a = Math.floor(Math.random() * 1000 - 500);
+        const b = Math.floor(Math.random() * 1000 - 500);
+        return new Problem(`${a} - ${b}`, `${a - b}`);
+    } else if (rand == 2) {
+        // multiplication
+        const a = Math.floor(Math.random() * 200 - 100);
+        const b = Math.floor(Math.random() * 200 - 100);
+        return new Problem(`${a} × ${b}`, `${a * b}`);
+    } else {
+        const a = Math.floor(Math.random() * 100 - 50);
+        const b = Math.floor(Math.random() * 100 - 50);
+        return new Problem(`${a * b} ÷ ${b}`, `${a}`);
+    }
+}
+
+function sendProblem(socket, room, problem) {
+    socket.emit('newProblemMe', problem);
+    socket.to(room).emit('newProblemThem', problem);
+}
+
 io.on('connection', (socket) => {
     console.log('A user connected.');
     const socketId = socket.id;
     let intervalId = null;
+    let room = null;
     socket.on('username', (username) => {
         userOfSocketId.set(socketId, username);
         socketIdOfUser.set(username, socketId);
@@ -102,26 +151,33 @@ io.on('connection', (socket) => {
             const user2 = usersWaiting[0];
             const socket2 = socketOfUser.get(user2);
             usersWaiting.shift();
-            usersInMatch.set(user1,user2);
-            usersInMatch.set(user2,user1);
-            const roomNum = usersInMatch.size / 2;
-            const room = `room ${roomNum}`;
+            usersInMatch.set(user1, user2);
+            usersInMatch.set(user2, user1);
+            room = `room ${usersInMatch.size / 2}`;
             socket.join(room);
             socket2.join(room);
+            roomOfSocket.set(socket, room);
+            roomOfSocket.set(socket2, room);
+            let newProblem = createProblem();
+            problemDataOfRoom.set(room, [new ProblemData(newProblem)]);
             socket.emit('inGame', { user1, user2 });
             io.to(socketIdOfUser.get(user2)).emit('inGame', { user1: user2, user2: user1 });
-            let time = 5;
+            let time = 3;
             io.to(room).emit('countdown', time);
             let countdown = true;
             intervalId = setInterval(() => {
                 if (countdown) {
+                    // during countdown
                     time--;
                     io.to(room).emit('countdown', time);
                     if (time == 0) {
                         countdown = false;
                         time = 120;
+                        io.to(room).emit('newProblemMe', newProblem);
+                        io.to(room).emit('newProblemThem', newProblem);
                     }
                 } else {
+                    // in game
                     if (time == 0) clearInterval(intervalId);
                     else {
                         io.to(room).emit('timeLeft', time);
@@ -129,6 +185,35 @@ io.on('connection', (socket) => {
                     }
                 }
             }, 1000);
+        }
+    });
+    socket.on('problemSolved', (problem) => {
+        if (room == null) {
+            room = roomOfSocket.get(socket);
+            if (room == null) return;
+        }
+        let problemData = problemDataOfRoom.get(room);
+        let idx = problemData.findIndex((p) => p.problem.id == problem.id);
+        if (problemData[idx].solvedBy == 0) {
+            problemData[idx].solvedBy++;
+            if (problemData.length <= idx + 1) {
+                const newProblem = createProblem();
+                const newProblemData = new ProblemData(newProblem);
+                problemData.push(newProblemData);
+                sendProblem(socket, room, newProblem);
+            } else {
+                sendProblem(socket, room, problemData[idx + 1].problem);
+            }
+        } else {
+            problemData.splice(0, idx + 1);
+            if (problemData.length == 0) {
+                const newProblem = createProblem();
+                const newProblemData = new ProblemData(newProblem);
+                problemData.push(newProblemData);
+                sendProblem(socket, room, newProblem);
+            } else {
+                sendProblem(socket, room, problemData[0].problem);
+            }
         }
     })
 });
